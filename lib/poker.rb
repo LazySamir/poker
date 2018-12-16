9f2214300fecf3fc205baad03e53920bc8559022
Samir Soormally <samirsoormally@gmail.com>
2018-12-16 22:21:33 +0000

    feat: added scoring for hands

--- lib/poker.rb
+++ lib/poker.rb
@@ -1,6 +1,8 @@
 class Poker
   # will need to instantiate with a player later
   # will instantiate with deck instance, now: hardcode
+  CARD_SCORES = { 'AH' => 1, '2H' => 2, '3H' => 3, '4H' => 4, '5H' => 5, '6H' => 6, '7H' => 7, '8H' => 8, '9H' => 9, '10H' => 10, 'JH' => 11, 'QH' => 12, 'KH' => 13, 'AD' => 1, '2D' => 2, '3D' => 3, '4D' => 4, '5D' => 5, '6D' => 6, '7D' => 7, '8D' => 8, '9D' => 9, '10D' => 10, 'JD' => 11, 'QD' => 11, 'KD' => 13, 'AC' => 1, '2C' => 2, '3C' => 3, '4C' => 4, '5C' => 5, '6C' => 6, '7C' => 7, '8C' => 8, '9C' => 9, '10C' => 10, 'JC' => 11, 'QC' => 12, 'KC' => 13, 'AS' => 1, '2S' => 2, '3S' => 3, '4S' => 4, '5S' => 5, '6S' => 6, '7S' => 7, '8S' => 8, '9S' => 9, '10S' => 10, 'JS' => 11, 'QS' => 12, 'KS' => 13}
+
   def initialize(no_players, handsize)
     @deck = [
       'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH',
@@ -12,11 +14,13 @@ class Poker
     @no_players = no_players
     @handsize = handsize
     @all_hands = []
+    @hand_scores = []
   end
 
   def deal_cards
     create_empty_hands
     @all_hands.each do |hand|
+      puts hand
       # repeat until array[element].length == handsize
       until hand.length == @handsize do
         # add a random card from the deck to first element
@@ -24,6 +28,13 @@ class Poker
         # NEED TO REMOVE CARD FROM DECK
       end
     end
+  end
+
+  def score_hands
+    @all_hands.each do |hand|
+      score_hand(hand)
+    end
+    @hand_scores
   end
 
   def show_hands
@@ -34,10 +45,20 @@ private
 
   def create_empty_hands
     # make array with number of elements equal to no_players(array of arrays)
-    @handsize.times { @all_hands.push([]) }
+    @handsize.times do
+      @all_hands.push([])
+    end
   end
 
   def pick_random_card
     @deck.sample
+  end
+
+  def score_hand(hand)
+    score = 0
+    hand.each do |card|
+      score += CARD_SCORES[card]
+    end
+    @hand_scores << score
   end
 end
--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -2,6 +2,8 @@ describe Poker do
   subject(:poker)   { Game.new(2, 4) }
 
   describe '#deal_cards' do
-
+    # randomly chosen
+    # array length matches players and handsize
+    # card is removed from deck
   end
 end
