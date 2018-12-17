ab5b2182f14af24d78ba98381a85f7a1eb7c53bf
Samir Soormally <samirsoormally@gmail.com>
2018-12-17 09:41:23 +0000

    refactor: removed redundant feature test

--- spec/features/user_can_customize_game_spec.rb
+++
@@ -1,7 +0,0 @@
-feature 'user can customize game' do
-  scenario 'user can enter players and handsize' do
-    enter_valid_details
-    click_button 'Play'
-    expect(page.status_code).to be(200)
-  end
-end
